4fcb6f4920975e7377566a4436729f6ba363a782
coop <coop@opennet-32-252.uhnres.utoronto.ca>
2019-05-17 16:14:56 -0400

    commit number 2

--- app/index.js
+++ app/index.js
@@ -7,6 +7,8 @@ const path = __dirname + '/views/';
 app.get('/', function(req, res){
         res.sendFile(path + 'index.html');
         })
+// Setup the static location for images <- so express knows this is where static files are
+app.use(express.static(__dirname + '/public'));
 
 app.listen(port, function(){
            console.log('listening on: ' + port);

0f0874d13ed11c11b1c69542496dc2a08a2d00ab
coop <coop@opennet-32-252.uhnres.utoronto.ca>
2019-05-21 15:58:04 -0400

    new stuff

--- app/public/main.js
+++ app/public/main.js
@@ -1,35 +1,38 @@
 
 $(document).ready(function () {
 	
-	var professions = ['Mathematician', 'Computer Scientist', 'Logician', 'Cryptanalyst', 'Philospher'];
 
+	//Animated description of turing's porfessions
+	var professions = ['Mathematician', 'Inventor', 'Computer Scientist', 'Logician', 'Cryptanalyst', 'Philospher'];
 	var elem = document.getElementById('changingProfessions');
-
 	var id = setInterval(frame, 1);
 	var pos = 0;
 	var counter = 0;
-	const MAX = 400
-	//var accel = 0;
-  	
+	const MAX = 100;
+	var transitionRegion = MAX / 8;
+
   	function frame() {
   		if (pos >= MAX){
   			counter ++;
-  			if (counter == 5){
+  			if (counter >= professions.length){
   				counter = 0;
   			}
   			elem.innerHTML = professions[counter];
   			pos = 0;
   		}
-  		pos ++;
+  		pos += 0.25;
   		elem.style.marginLeft = pos/5 + "px"; 
-  		if(pos < (MAX/2)){
-  			elem.style.opacity = 1 * (pos/MAX); 
-  		} else {
-  			elem.style.opacity = 1 * (MAX/pos);
-  		}
-  		
-      	
+
+  		if(pos < transitionRegion){
+  			elem.style.opacity = Math.pow(pos,1.01) / transitionRegion;
+  		} else if (pos > transitionRegion * 3) {
+  			elem.style.opacity = Math.pow(MAX - pos,1.01) / transitionRegion;
+  		}	
   	}
+
+
+
+
 
 });
 

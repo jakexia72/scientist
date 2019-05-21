0ec677ca010b6641b7c7c8fcfe2e3935cb18e628
coop <coop@opennet-32-252.uhnres.utoronto.ca>
2019-05-21 12:17:48 -0400

    for Sharma

--- app/public/main.js
+++ app/public/main.js
@@ -1,1 +1,35 @@
 
+$(document).ready(function () {
+	
+	var professions = ['Mathematician', 'Computer Scientist', 'Logician', 'Cryptanalyst', 'Philospher'];
+
+	var elem = document.getElementById('changingProfessions');
+
+	var id = setInterval(frame, 1);
+	var pos = 0;
+	var counter = 0;
+	const MAX = 400
+	//var accel = 0;
+  	
+  	function frame() {
+  		if (pos >= MAX){
+  			counter ++;
+  			if (counter == 5){
+  				counter = 0;
+  			}
+  			elem.innerHTML = professions[counter];
+  			pos = 0;
+  		}
+  		pos ++;
+  		elem.style.marginLeft = pos/5 + "px"; 
+  		if(pos < (MAX/2)){
+  			elem.style.opacity = 1 * (pos/MAX); 
+  		} else {
+  			elem.style.opacity = 1 * (MAX/pos);
+  		}
+  		
+      	
+  	}
+
+});
+
